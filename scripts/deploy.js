c67e8f8dc946355f87d7713bec71fb628cc4a39a
dam than <thandam2009@gmail.com>
2024-09-07 16:00:25 +0700

    task - 01 simple contractt

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  const contract = await hre.ethers.deployContract("Swisstronik", [
+    "Hello Swisstronik from Julia with love",
+  ]);
+
+  await contract.waitForDeployment();
+
+  console.log(`Swisstronik contract deployed to ${contract.target}`);
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
